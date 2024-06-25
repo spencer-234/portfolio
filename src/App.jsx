f74dbe6ae07f38cde1a6465e32df232d97ea3aed
Spencer Evans <spencerevans234@gmail.com>
2024-06-25 17:14:47 -0400

    add initial home section and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import "./app.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Title from "./components/Title/Title";
 import Home from "./sections/Home/Home";
+import { VisibilityContext } from "./context/VisibilityContext.jsx";
+import { useContext } from "react";
 
 const App = () => {
+
+  const { visibility } = useContext(VisibilityContext);
+
   return (
     <>
       <Title />
-      <Navbar />
-      <main>
-        <Home />
-      </main>
+      {!visibility && (
+        <>
+          <Navbar />
+          <main>
+            <Home />
+          </main>
+        </>
+      )}
     </>
   )
 };
--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,9 +1,30 @@
 import "./home.scss";
+import DevSvg from "../../components/DevSvg/DevSvg";
+import github from "/assets/icons/github.png";
+import linkedIn from "/assets/icons/linkedIn.png";
 
 const Home = () => {
     return (
         <section className="home">
-            Hello
+            <div className="glass">
+                <div className="home-container">
+                    <div className="intro">
+                        <h1>Spencer Evans</h1>
+                        <span>Web Developer</span>
+                        <div className="link-container">
+                            <a href="https://github.com/spencer-234" rel="noopener noreferrer">
+                                <img src={github} alt="github-account-link" />
+                            </a>
+                            <a href="www.linkedin.com/in/spencer-evans-11155a28b" rel="noopener noreferrer">
+                                <img src={linkedIn} alt="linked-in-account-link" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="dev-svg">
+                        <DevSvg />
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
